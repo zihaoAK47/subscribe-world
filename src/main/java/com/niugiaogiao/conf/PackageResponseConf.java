4e0033d0f67d9bda7aee9a50a256d683ddc7063a
niugiaogiao <zihaoAK47@163.com>
2023-02-01 13:35:03 +0800

    Rename class file.

--- src/main/java/com/niugiaogiao/conf/PackageResponseConf.java
+++ src/main/java/com/niugiaogiao/conf/PackageResponseConf.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice(basePackages = {"com.niugiaogiao.modules"})
-class ResponsePackageConf implements ResponseBodyAdvice<Object> {
+class PackageResponseConf implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
         return true;
