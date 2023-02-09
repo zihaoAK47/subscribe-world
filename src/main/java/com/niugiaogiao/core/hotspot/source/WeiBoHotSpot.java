abe744ce240fb4f2901649d38aeaa777c8c5f5b4
niugiaogiao <zihaoAK47@163.com>
2023-02-09 16:13:42 +0800

    Change WEB_BO hotSpot request.

--- src/main/java/com/niugiaogiao/core/hotspot/source/WeiBoHotSpot.java
+++ src/main/java/com/niugiaogiao/core/hotspot/source/WeiBoHotSpot.java
@@ -3,7 +3,10 @@ package com.niugiaogiao.core.hotspot.source;
 import com.niugiaogiao.core.enums.Platform;
 import com.niugiaogiao.core.hotspot.AbstractHotSpot;
 import com.niugiaogiao.core.hotspot.HotSpotAPiSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 微博热点数据
@@ -11,9 +14,18 @@ import org.springframework.stereotype.Service;
 @Service
 class WeiBoHotSpot extends AbstractHotSpot implements HotSpotAPiSource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     private static final String wbHotSpot = "https://weibo.com/ajax/statuses/hot_band";
 
     public WeiBoHotSpot() {
         super(wbHotSpot, Platform.WEI_BO);
+    }
+
+    @Override
+    public String downDataSource() {
+        ResponseEntity<String> response = restTemplate.getForEntity(wbHotSpot, String.class);
+        return response.getBody();
     }
 }
